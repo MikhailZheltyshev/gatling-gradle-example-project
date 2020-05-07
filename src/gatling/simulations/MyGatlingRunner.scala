8c5a25320593e8323eb4b84c2fb38f6b7f7f32a2
Mike Zheltyshev <mihalich93@bk.ru>
2020-05-07 17:52:27 +0300

    project files added

---
+++ src/gatling/simulations/MyGatlingRunner.scala
@@ -0,0 +1,17 @@
+import io.gatling.app.Gatling
+import io.gatling.core.config.GatlingPropertiesBuilder
+import simulations.AddPauseTime
+
+object MyGatlingRunner {
+
+  def main(args: Array[String]): Unit = {
+
+    val simClass = classOf[AddPauseTime].getName
+
+    val props = new GatlingPropertiesBuilder
+    props.simulationClass(simClass)
+
+    Gatling.fromMap(props.build)
+  }
+
+}
